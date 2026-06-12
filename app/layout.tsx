e59d9a11a64ffe7066fc1bf64efe1aac48b7e993
stevedelcourt <steven.delcourt@mentivis.com>
2026-06-12 16:13:52 +0200

    feat: Google Consent Mode v2 defaults before GTM load

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,24 @@ export default function RootLayout({
   return (
     <html lang="fr">
       <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag() { dataLayer.push(arguments); }
+              gtag('consent', 'default', {
+                'ad_storage': 'denied',
+                'ad_user_data': 'denied',
+                'ad_personalization': 'denied',
+                'analytics_storage': 'denied',
+                'functionality_storage': 'granted',
+                'personalization_storage': 'denied',
+                'security_storage': 'granted',
+                'wait_for_update': 500,
+              });
+            `,
+          }}
+        />
         <Script
           id="gtm"
           strategy="afterInteractive"
