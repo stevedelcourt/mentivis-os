4bcf9bfa408768d7e51c4ed6d56084a7c84fbe01
stevedelcourt <steven.delcourt@mentivis.com>
2026-08-17 12:41:06 +0200

    pdf-unlock: post to /api/demo/ to avoid trailing-slash redirect

--- components/pdf-unlock.tsx
+++ components/pdf-unlock.tsx
@@ -56,7 +56,7 @@ function PdfUnlockModal({
     setLoading(true);
     setError("");
     try {
-      const res = await fetch("/api/demo", {
+      const res = await fetch("/api/demo/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
